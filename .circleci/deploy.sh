2889de8cfa597773441797b00a5a72a02b5a9d49
Rafael Ferreira <raf@uvasoftware.com>
2019-11-15 07:23:54 -0500

    Dep upgrades
    Upgraded lambda runtime to node 10
    Upgraded deps with vulnerabilities

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -17,9 +17,9 @@ VERSION=$( node -pe "require('./package.json').version")
 make build package || exit 100
 
 aws serverlessrepo create-application-version \
- --application-id arn:aws:serverlessrepo:us-east-1:${ACCOUNT_ID}:applications/${APPLICATION} \
- --semantic-version ${VERSION} \
- --source-code-url https://github.com/uvasoftware/scanii-lambda/releases/tag/v${VERSION} \
+ --application-id arn:aws:serverlessrepo:us-east-1:"${ACCOUNT_ID}":applications/${APPLICATION} \
+ --semantic-version "${VERSION}" \
+ --source-code-url https://github.com/uvasoftware/scanii-lambda/releases/tag/v"${VERSION}" \
  --template-body file://scanii-lambda.yaml >/dev/null || exit 99
 
 echo "SAM application ${APPLICATION} version ${VERSION} published!"
@@ -27,11 +27,11 @@ echo "SAM application ${APPLICATION} version ${VERSION} published!"
 # tag repo
 git config --global user.email "circleci@uvasoftware.com"
 git config --global user.name "CircleCI"
-git tag -a v${VERSION} -m "Release by CircleCI v${VERSION}"
-git push origin v${VERSION}
+git tag -a v"${VERSION}" -m "Release by CircleCI v${VERSION}"
+git push origin v"${VERSION}"
 
 # bumping version
-npm --no-git-tag-version version patch
+npm --no-git-tag-version version minor
 VERSION=$( node -pe "require('./package.json').version")
 echo "next version is: $VERSION"
 
