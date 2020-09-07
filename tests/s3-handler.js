9e49baf158e11e943cfc047b051eff8c474b1c1c
Rafael Ferreira <raf@uvasoftware.com>
2020-09-07 00:32:14 -0400

    Multiple improvements (#47)
    * Obfuscate key
    
    Fixes #46
    
    * Added retry logic
    
    Fixes #40
    
    * Tweaking config
    
    * Migrate to github actions
    
    * Dropped node v10

--- tests/s3-handler.js
+++ tests/s3-handler.js
@@ -1,4 +1,5 @@
 const handler = require('../lib/s3-handler').handler;
+const scanii = require('../lib/client');
 const assert = require('assert');
 const it = require("mocha/lib/mocha.js").it;
 const describe = require("mocha/lib/mocha.js").describe;
@@ -7,25 +8,31 @@ const afterEach = require("mocha/lib/mocha.js").afterEach;
 const AWS = require('aws-sdk');
 const nock = require('nock');
 const CONFIG = require('../lib/config').CONFIG;
+const sinon = require('sinon');
+const {defaults} = require("../lib/config");
 
 describe('S3 handler tests', () => {
+  const sandbox = sinon.createSandbox();
+
   beforeEach(() => {
+    sandbox.spy(scanii.ScaniiClient);
 
     // wrapping some fakes around the AWS sdk:
     AWS.S3.prototype.getSignedUrl = () => 'https://example.com/1234?q=124';
     CONFIG.CALLBACK_URL = "https://example.com/callback/";
     CONFIG.KEY = "k";
     CONFIG.SECRET = "s";
+    CONFIG.MAX_ATTEMPTS = 1;
+    CONFIG.MAX_ATTEMPT_DELAY_MSEC = 1_000;
   });
 
   afterEach(() => {
-    CONFIG.CALLBACK_URL = null;
-    CONFIG.KEY = null;
-    CONFIG.SECRET = null;
+    nock.cleanAll();
+    sandbox.restore();
+    defaults();
   });
 
   it('should process a create object event', async () => {
-
     nock('https://api.scanii.com')
       .post('/v2.1/files/fetch')
       .reply(202, Buffer.from("{\"id\":\"12356789\"}"), {"Location": "https://api.scanii.com/v2.1/files/1234"});
@@ -172,6 +179,57 @@ describe('S3 handler tests', () => {
       assert(error === null, "there should be no errors");
       assert(result.statusCode === 500, "should return the file id");
       assert(result.body.includes("bucket not present"));
+    });
+  });
+  it('should obfuscate the object key', async () => {
+    nock('https://api.scanii.com')
+      .post('/v2.1/files/fetch')
+      .reply(202, Buffer.from("{\"id\":\"12356789\"}"), {"Location": "https://api.scanii.com/v2.1/files/1234"});
+
+    const spy = sinon.spy(scanii.ScaniiClient.prototype, 'fetch');
+
+    await handler({
+      "Records": [
+        {
+          "eventVersion": "2.0",
+          "eventSource": "aws:s3",
+          "awsRegion": "us-west-2",
+          "eventTime": "2015-10-01T23:28:54.280Z",
+          "eventName": "ObjectCreated:Put",
+          "userIdentity": {
+            "principalId": "AWS:principal"
+          },
+          "requestParameters": {
+            "sourceIPAddress": "98.167.155.191"
+          },
+          "responseElements": {
+            "x-amz-request-id": "EEC943B096DE3DF9",
+            "x-amz-id-2": "W/myEjyXFBsOA6N0byxW0tOxMA4m1fmv9KAVcovvG0nD9W1s5aX5+Wx61tlCop8LbZAw1Nz0mnc="
+          },
+          "s3": {
+            "s3SchemaVersion": "1.0",
+            "configurationId": "948c2c1a-a028-4564-93fc-76cea7622633",
+            "bucket": {
+              "name": "scanii-mu",
+              "ownerIdentity": {
+                "principalId": "principal"
+              },
+              "arn": "arn:aws:s3:::scanii-mu"
+            },
+            "object": {
+              "key": "Screen+Shot+2016-01-19+at+7.24.37+PM.png",
+              "size": 519,
+              "eTag": "aa1e5c8a6a07217c25f55aa8e96ea37a",
+              "sequencer": "00560DC1B62F962FCD"
+            }
+          }
+        }
+      ]
+    }, {}, () => {
+      "use strict";
+      assert(spy.calledOnce)
+      assert((spy.getCall(0).args[2]['key-sha1'] === '8fd1943cbc9bf22f0b7f64aef9015f6494b81846'))
+      assert((spy.getCall(0).args[2]['key'] === undefined))
     });
   });
 });
