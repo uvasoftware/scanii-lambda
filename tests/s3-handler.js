2c85971810b02518cdd792343f3d346d36aa85ef
BernardoScantamburlo <bernardo@uvasoftware.com>
2024-08-18 10:31:21 -0400

    Adds logic to stop processing from failing

--- tests/s3-handler.js
+++ tests/s3-handler.js
@@ -180,5 +180,53 @@ describe('S3 handler tests', () => {
       assert(result.body.includes("bucket not present"));
     });
   });
+  it('should fail to process a directory', async () => {
+
+    nock('https://api-us1.scanii.com')
+      .post('/v2.2/files/fetch')
+      .reply(202, Buffer.from("{\"id\":\"12356789\"}"), {"Location": "https://api-us1.scanii.com/v2.2/files/1234"});
+
+    await handler({
+      "Records": [
+        {
+          "eventVersion": "2.0",
+          "eventSource": "aws:s3",
+          "awsRegion": "us-west-2",
+          "eventTime": "2015-10-01T23:28:54.280Z",
+          "eventName": "ObjectCreated:Put",
+          "userIdentity": {
+            "principalId": "AWS:principal"
+          },
+          "requestParameters": {
+            "sourceIPAddress": "98.167.155.191"
+          },
+          "responseElements": {
+            "x-amz-request-id": "EEC943B096DE3DF9",
+            "x-amz-id-2": "W/myEjyXFBsOA6N0byxW0tOxMA4m1fmv9KAVcovvG0nD9W1s5aX5+Wx61tlCop8LbZAw1Nz0mnc="
+          },
+          "s3": {
+            "s3SchemaVersion": "1.0",
+            "configurationId": "948c2c1a-a028-4564-93fc-76cea7622633",
+            "bucket": {
+              "name": "scanii-mu",
+              "ownerIdentity": {
+                "principalId": "principal"
+              },
+              "arn": "arn:aws:s3:::scanii-mu"
+            },
+            "object": {
+              "size": 519,
+              "eTag": "aa1e5c8a6a07217c25f55aa8e96ea37a",
+              "key": "Screen+Shot+2016-01-19+at+7.24.37+PM.png",
+              "sequencer": "00560DC1B62F962FCD"
+            }
+          }
+        }
+      ]
+    }, {}, (error, result) => {
+      assert(error === null, "there should be no errors");
+      assert(result.body.includes("cannot process directory"));
+    });
+  });
 });
 
